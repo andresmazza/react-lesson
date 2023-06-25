716e597ad7e528773d1f7afd86c7714eb52e0d4f
Andres Mazza <andres.mazza@gmail.com>
2023-06-25 22:30:26 +0000

    dev

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,11 +1,15 @@
-
 import { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
-const ExpenseItem = (props) => {  
+const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => { 
+    setTitle("lalal");
+    console.log(title);
+  };
 
   return (
     <Card className="expense-item">
@@ -13,8 +17,8 @@ const ExpenseItem = (props) => {
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">$ {props.amount}</div>
+        <button onClick={clickHandler}>Change Title </button>
       </div>
-
     </Card>
   );
 };
